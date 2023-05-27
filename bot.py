66d24ff1073f9659d32119b0e0ef3fa8ed6e40cf
Boost <74883609+AngleBoost@users.noreply.github.com>
2023-05-27 02:18:09 -0500

    Update bot.py
    Updated /play with spotify playing music tracks

--- bot.py
+++ bot.py
@@ -118,6 +118,30 @@ async def play_command(interaction, link: str):
                 # If the bot is not in a voice channel, prompt the user to join a voice channel
                 embed.set_field_at(0, name="Status", value="❗ Please join a voice channel first.")
                 await interaction.edit_original_response(embed=embed)
+        elif "open.spotify.com/track" in link:
+            # If the link is a Spotify track, add it to the queue
+            sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+            track_id = link.split('/')[-1].split('?')[0]
+            track = sp.track(track_id)
+            artists = ', '.join([artist['name'] for artist in track['artists']])
+            title = f"{artists} - {track['name']}"
+            if 'preview_url' in track and track['preview_url'] is not None:
+                queue.append((title, track['preview_url']))
+            
+            if voice_client and (not voice_client.is_playing() and not voice_client.is_paused()):
+                # If the bot is in a voice channel and no song is currently playing or paused, play the requested song immediately
+                title, url = queue.pop(0)
+                voice_client.play(discord.FFmpegPCMAudio(url))
+                embed.set_field_at(0, name="Status", value=f"🎵 Now playing: {title}")
+                await interaction.edit_original_response(embed=embed)
+            elif voice_client:
+                # If the bot is in a voice channel but a song is already playing or paused, add the requested song to the queue
+                embed.set_field_at(0, name="Status", value=f"🎵 Added to the queue: {link}")
+                await interaction.edit_original_response(embed=embed)
+            else:
+                # If the bot is not in a voice channel, prompt the user to join a voice channel
+                embed.set_field_at(0, name="Status", value="❗ Please join a voice channel first.")
+                await interaction.edit_original_response(embed=embed)
         
         else:
             # If the link is a single song, add it to the queue
